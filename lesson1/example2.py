e86ec1c4daf50345f0ed3289d81722b069a4ca8f
hardentoo <hardenedgentoo@gmail.com>
2017-04-08 16:01:24 +0300

    Update

---
+++ lesson1/example2.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2017 bsd <bsd@Gentoo>
+#
+# Distributed under terms of the MIT license.
+
+age = 17
+
+if age < 20:
+    print('Good')
+else:
+    print('Bad')
+
+food = ['еда1', 'еда2', 'еда3', 'еда4']
+
+for f in food:
+    print(f[3])
